b37b70cfaf7eead38fb2986784d65489b3dd90cd
kzz <kzz>
2026-07-01 00:25:09 +0300

    Add : Files to project

---
+++ .angular/cache/21.2.17/admin/vite/deps_ssr/@angular_ssr.js
@@ -0,0 +1,40 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  AngularAppEngine,
+  IS_DISCOVERING_ROUTES,
+  InlineCriticalCssProcessor,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  destroyAngularServerApp,
+  extractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig,
+  provideServerRendering,
+  setAngularAppEngineManifest,
+  setAngularAppManifest,
+  withAppShell,
+  withRoutes
+} from "./chunk-OE2XD3CU.js";
+import "./chunk-JOZYRRSY.js";
+import "./chunk-TUHNKYIE.js";
+import "./chunk-6IW5O36F.js";
+import "./chunk-7VO7PY3Q.js";
+import "./chunk-6DU2HRTW.js";
+export {
+  AngularAppEngine,
+  IS_DISCOVERING_ROUTES,
+  PrerenderFallback,
+  RenderMode,
+  createRequestHandler,
+  provideServerRendering,
+  withAppShell,
+  withRoutes,
+  InlineCriticalCssProcessor as ɵInlineCriticalCssProcessor,
+  destroyAngularServerApp as ɵdestroyAngularServerApp,
+  extractRoutesAndCreateRouteTree as ɵextractRoutesAndCreateRouteTree,
+  getOrCreateAngularServerApp as ɵgetOrCreateAngularServerApp,
+  getRoutesFromAngularRouterConfig as ɵgetRoutesFromAngularRouterConfig,
+  setAngularAppEngineManifest as ɵsetAngularAppEngineManifest,
+  setAngularAppManifest as ɵsetAngularAppManifest
+};
